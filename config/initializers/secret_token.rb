1e9c6feaf622eb160fee63bb0badd5cb1b80e0ce
vagrant <vagrant@precise64.(none)>
2014-03-11 05:41:52 -0300

    Initialiiizeee!

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TodoList::Application.config.secret_key_base = 'e081ea20b636ae1e568857e4b5905adc2059df749cd25514ba225341f15b98ff5934a72dbc21568d1ab9d83747abafa2d096bdcce946d66ebe104c29b2013436'
---
+++ scopes.js
@@ -0,0 +1,13 @@
+function displayMessage(){
+	console.log("Hello there", message);	
+}
+
+var message = "magno";
+
+displayMessage();
+
+function displayMessage(){
+	console.log("hello there", message);
+}
+
+displayMessage();
